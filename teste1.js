9825532d44c29ca65599610ee72819d3d3a939e9
dionata2007 <dionata123456789ad@gmail.com>
2026-06-05 20:58:24 -0300

    Mais uma aula

---
+++ teste1.js
@@ -0,0 +1,51 @@
+//const arrayVazia = [];
+ 
+//console.log(arrayVazia)
+//console.log(arrayVazia.length)
+//resultados
+//[]
+//0
+
+//const arrayVazia = [];
+//console.log(arrayVazia[0])
+//resultados
+//undefined
+
+//const arrayVazia = [,,,];
+//console.log(arrayVazia.length)
+//console.log(arrayVazia[0])
+//console.log(arrayVazia[1])
+//console.log(arrayVazia[2])
+//const arrayVazia = [,,,];
+//console.log(arrayVazia.length)
+//resultados
+//3
+//undefined
+//undefined
+//undefined
+
+//arrayVazia.push(50)
+//console.log(arrayVazia)
+//console.log(arrayVazia.length)
+//resultados
+///3
+///[ <3 empty items>, 50 ]
+///4
+
+//const arrayVazio = [];
+//const arrayNaoVazio = [1, 2, 3];
+//console.log(Boolean(arrayVazio)); // Saída: true - Mesmo sendo um array vazio, é "truthy"
+//console.log(Boolean(arrayNaoVazio)); // Saída: true - Um array com elementos também é "truthy"
+//resultados
+//true
+//true
+
+//(9.999).toFixed(2); // toFixed é um método de Number
+//'string'.toUpperCase(); // toUpperCase é um método de String
+//[1, 2, 3].push(4); // push é um método de Array
+
+//const lista = [7, 13, 'JavaScript', 'Alura', true];
+//lista.push(false);
+//console.log(lista.length); // 6
+//resultado
+//6
